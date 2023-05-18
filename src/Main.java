118c1241a316e2f3e7647b64c6e50210a5dd53ab
Dmitry Kurganov <dimacup30@gmail.com>
2023-05-18 19:26:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int payment = 8000;
+        int bonus = payment/20;
+
+        System.out.println("За билет стоимостью " + payment + " рублей,будет начислено " + bonus + " бонусных миль");
+
+    }
+}
